aec329ea39ceccf243c6b569bce961e49bdeaebf
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-10-05 13:56:20 -0400

    Version 1.0 ---- This is the assnmt - incomplete.

---
+++ src/mathmethods/MathMethods.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mathmethods;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Chris Hitchcock - 05/10/16
+ * This program is a GUI program which shows math methods from the Math API
+ */
+public class MathMethods {
+    JFrame frame;
+    JButton enter;
+    JTextField prompt;
+    
+    public MathMethods()
+    {
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
